d1d95f603eff6ced22f70e64f797e13a00fb6e82
Bradkall <bkallgre@students.kennesaw.edu>
2021-07-26 12:43:39 -0400

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,24 @@
-
+print("What day is it?")
+day = input() # user_input
+print("Did you work out yesterday?")
+ans = input()
+if ans=="yes" or ans == "YES":
+    if day=="Monday" or day=="Thursday":
+        print("Time for chest and Tricepts!")
+    elif day == "Tuesday" or day=="Friday":
+        print("It's Back and Bicepts time!")
+    elif day=="Wednesday" or day=="Saturday":
+        print("LEG DAY!!")
+    elif day== "Sunday":
+        print("Lets go for a run!")
+    else:
+        print("What day was yesterday?")
+day = input()
+if day == "Monday" or day == "Thursday":
+    print("Chest and Tricepts but go a little harder!")
+elif day == "Tuesday" or day=="Friday":
+    print( "Back and Bicepts but put a little more into this one!")
+elif day == "Wednesday" or day=="Saturday":
+    print( "Time to SQUAT!")
+else:
+    print( "Chest and Tricepts with a nice run after!")
